a5b3c178f9a8863f54b823b79f7321236e697651
wuuu0325 <846948088@qq.com>
2019-07-14 23:14:55 +0800

    add 2 problems

---
+++ LC201907/No1025.cs
@@ -0,0 +1,83 @@
+﻿/*
+ 1025. Divisor Game
+Easy
+
+Alice and Bob take turns playing a game, with Alice starting first.
+
+Initially, there is a number N on the chalkboard.  On each player's turn, that player makes a move consisting of:
+
+Choosing any x with 0 < x < N and N % x == 0.
+Replacing the number N on the chalkboard with N - x.
+Also, if a player cannot make a move, they lose the game.
+
+Return True if and only if Alice wins the game, assuming both players play optimally.
+
+ 
+
+Example 1:
+
+Input: 2
+Output: true
+Explanation: Alice chooses 1, and Bob has no more moves.
+Example 2:
+
+Input: 3
+Output: false
+Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
+ 
+
+Note:
+
+1 <= N <= 1000
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC201907
+{
+    class No1025
+    {
+        //bool[] results_Recur = new bool[1001];
+        public bool DivisorGame_Recursion(int N)
+        {
+            if (N == 1)
+                return false;
+
+            for (int i = 1; i < N; i++)
+            {
+                if (N % i == 0 && DivisorGame_Recursion(N - i) == false)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool DivisorGame_DP(int N)
+        {
+            if (N == 1)
+                return false;
+
+            bool[] results = new bool[N + 1];
+            int input = 2;
+            while (input <= N)
+            {
+                for (int i = 1; i < input; i++)
+                {
+                    if (input % i == 0 && results[input - i] == false)
+                    {
+                        results[input] = true;
+                        break;
+                    }
+                }
+                input++;
+            }
+            return results[N];
+        }
+
+    }
+}
---
+++ LC201907/No264.cs
@@ -0,0 +1,50 @@
+﻿/*
+ 279. Perfect Squares
+Medium
+
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+Example 2:
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC201907
+{
+    public class No264
+    {
+        public int NthUglyNumber(int n)
+        {
+            if (n == 0)
+                return 0;
+            int t2 = 0, t3 = 0, t5 = 0;
+            int[] uglyNum = new int[n + 1];
+            uglyNum[0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                uglyNum[i] = Math.Min(uglyNum[t2] * 2, Math.Min(uglyNum[t3] * 3, uglyNum[t5] * 5));
+                if (uglyNum[i] == uglyNum[t2] * 2)
+                    t2++;
+                if (uglyNum[i] == uglyNum[t3] * 3)
+                    t3++;
+                if (uglyNum[i] == uglyNum[t5] * 5)
+                    t5++;
+            }
+            return uglyNum[n - 1];
+        }
+
+    }
+}
