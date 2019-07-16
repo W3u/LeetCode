5b0e453e53511505799a626b2b5d181f7e1b287b
wuuu0325 <846948088@qq.com>
2019-07-16 23:06:05 +0800

    20190716 NO.121

---
+++ LC201907/No121.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC201907
+{
+    class No121
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int left = 0, right = 0;
+            //int minIdx, maxIdx;
+            int maxProfit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[left] > prices[i])
+                {
+                    left = i;
+                }
+                if (prices[i] > prices[right] || left > right)
+                {
+                    right = i;
+                }
+                if (prices[right] - prices[left] > maxProfit)
+                {
+                    maxProfit = prices[right] - prices[left];
+                    //minIdx = left;
+                    //maxIdx = right;
+                }
+            }
+            return maxProfit;
+        }
+    }
+}
--- LC201907/Program.cs
+++ LC201907/Program.cs
@@ -10,8 +10,18 @@ namespace LC201907
     {
         static void Main(string[] args)
         {
-            No279 no279 = new No279();
-            no279.NumSquares_BFS_V2(12);
+            int[] vs = { 7, 2, 5, 3, 6, 4, 9, 1 };
+            No121 no121 = new No121();
+            no121.MaxProfit(vs);
+
+            //No264 no264 = new No264();
+            //no264.NthUglyNumber(10);
+
+            //No279 no279 = new No279();
+            //no279.NumSquares_BFS_V2(12);
+
+            //No1025 no1025 = new No1025();
+            //no1025.DivisorGame_Recursion(877);
         }
     }
 }
